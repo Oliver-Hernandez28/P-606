7871cd5a58be57615998f06f2fd119ba363c627f
Oliver Hernández Hernández <83966309+Oliver-Hernandez28@users.noreply.github.com>
2022-03-16 19:43:31 -0600

    Add files via upload

---
+++ Practica.php
@@ -0,0 +1,18 @@
+<?php
+if (isset($post['Solicitud'])) {
+  $Dirección=$post['Dir'];
+  $Fecha=$post['Date'];
+  $Servicio=$post['Ser'];
+  $Nombre=$post['Nom'];
+  $Municipio=$post['Mun'];
+  $Horario=$post['Hor'];
+}
+$Hii=INSERT INTO Solicitud(id_Solicitud, Direccion, Fecha, Servicio, Nombre, Municipio, Horario) values ('$Direccion','$Fecha', '$Servicio', '$Nombre', '$Municipio', '$Horario');
+
+$Hii=$conecta=query(Hii);
+if ($Mellow<0) {
+  echo "Solicitud Exitosa";
+  else {
+  }
+}
+?>
